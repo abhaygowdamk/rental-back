bb953364287715e020c2a1eb5c760213867f7be3
abhaygowdamk <121029459+abhaygowdamk@users.noreply.github.com>
2025-06-07 21:56:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://car-rental-backend-hgwt.onrender.com/",
+  origin: "https://rental-front-kappa.vercel.app//",
   credentials: true
 }));
 app.use(express.json());
